a641bcd4752df76d25b6d8b48583da6615f93dcf
snh3003 <shahsama542@gmail.com>
2021-12-28 22:48:12 +0530

    complete app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+
+  count: number = 0;
+
+  handleIncrement = () => {
+    this.count >= 9 ? alert("Max Limit!!!") : this.count++;
+  }
+
+  handleDecrement = () => {
+    this.count <= 0 ? alert("Min Limit!!!") : this.count--;
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
 }
